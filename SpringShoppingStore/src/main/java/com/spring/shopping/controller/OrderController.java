4d3c6a5a11ca53788638f13e315fe01bb70c0bb5
gitSpartanac <djole_serbia90@hotmail.com>
2015-12-06 11:18:10 +0100

    Mogucnost kupovine i za neprijavljene korisnike

--- SpringShoppingStore/src/main/java/com/spring/shopping/controller/OrderController.java
+++ SpringShoppingStore/src/main/java/com/spring/shopping/controller/OrderController.java
@@ -3,38 +3,50 @@ package com.spring.shopping.controller;
 import java.io.IOException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.Date;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-import javax.ws.rs.client.Client;
-import javax.ws.rs.client.ClientBuilder;
-import javax.ws.rs.client.Entity;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
 
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+//import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.client.RestTemplate;
 
+import com.google.gson.Gson;
 import com.spring.shopping.controller.constants.ControllerConstants;
 import com.spring.shopping.model.AddressForm;
+import com.spring.shopping.model.Auth;
 import com.spring.shopping.model.CreditCardForm;
 import com.spring.shopping.model.Customer;
 import com.spring.shopping.model.Order;
 import com.spring.shopping.model.OrderItemExtended;
 import com.spring.shopping.model.Product;
+import com.spring.shopping.model.TransactionSale;
 import com.spring.shopping.service.CartData;
 import com.spring.shopping.service.CartService;
 import com.spring.shopping.service.MailSenderService;
 import com.spring.shopping.service.OrderService;
 import com.spring.shopping.service.PaymentService;
 import com.spring.shopping.util.SessionUtils;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.MediaType;
 
 @Controller
 public class OrderController {
@@ -106,54 +118,41 @@ public class OrderController {
 		  .request(MediaType.APPLICATION_JSON_TYPE)
 		  .post(payload);
 
-			
-		String saleResponse = response.readEntity(String.class);
-		System.out.println("body:" + saleResponse);
+		
+		System.out.println("body:" + response.readEntity(String.class));
 		System.out.println("status: " + response.getStatus());
 		System.out.println("headers: " + response.getHeaders());
-		
-		
-		
-		JSONObject jsonSaleResponse = new JSONObject(saleResponse);
-		String transactionId = (String) jsonSaleResponse.get("transactionid");
-		int status = (Integer) jsonSaleResponse.get("status");
-		
-		
-		
 		session = SessionUtils.createSession(request);
-		
 		// Retrieve Details about the Cart,Customer and Address Details
 		// used to create detailed Order
 		Customer customer = SessionUtils.getSessionVariables(request,
 				"customer");
-		AddressForm address = SessionUtils.getSessionVariables(request,
-				"address");
-
-
-		Order order = orderService.createOrder(cartService, customer, address, 	request, transactionId, Order.mappStatus(status));
+		//AddressForm address = SessionUtils.getSessionVariables(request,"address");
+		Order order=null;
+		if(customer!=null)
+		{
+			order = orderService.createOrder(cartService, customer, null,request);
+			payAmountByCreditCard(creditCardForm, request);
 	
-		
-		
-//		orderService.saveTransactionId
-	
-		payAmountByCreditCard(creditCardForm, request);
-
-		SessionUtils.setSessionVariables(order, request, "orderDetails");
-		List<Product> productsList = orderService.getAllOrderItems(order);
-		StringBuffer sb = new StringBuffer();
-		sb.append("Hello " + customer.getUserName() + "\n");
-		sb.append("Thank you for shopping at eShopper.Happy Shopping!!\n");
-		sb.append("OrderId-" + order.getOrderId() + "/n");
-		sb.append("Products-/n");
-		for (Product p : productsList) {
-			sb.append(p.getName() + "  Rs." + p.getPrice() + "\n");
+			SessionUtils.setSessionVariables(order, request, "orderDetails");
+			List<Product> productsList = orderService.getAllOrderItems(order);
+			StringBuffer sb = new StringBuffer();
+			sb.append("Hello " + customer.getUserName() + "\n");
+			sb.append("Thank you for shopping at eShopper.Happy Shopping!!\n");
+			sb.append("OrderId-" + order.getOrderId() + "/n");
+			sb.append("Products-/n");
+			for (Product p : productsList) {
+				sb.append(p.getName() + "  Rs." + p.getPrice() + "\n");
+			}
+			sb.append("Your Order Status is: " + order.getOrderStatus());
+			sb.append("You will get further notification.Once your order is processed");
+			mailSenderService.sendEmail(customer.getEmailAddress(),
+					customer.getUserName(), sb.toString(),
+					"Order Confirmation for " + customer.getUserName());
+				return "redirect:/order";
 		}
-		sb.append("Your Order Status is: " + order.getOrderStatus());
-		sb.append("You will get further notification.Once your order is processed");
-		mailSenderService.sendEmail(customer.getEmailAddress(),
-				customer.getUserName(), sb.toString(),
-				"Order Confirmation for " + customer.getUserName());
-		return "redirect:order";
+		else
+			return "successPayment";
 	}
 
 	public void payAmountByCreditCard(CreditCardForm creditCardForm,
@@ -173,9 +172,8 @@ public class OrderController {
 		CartData cartData = SessionUtils.getSessionVariables(request,
 				ControllerConstants.CART);
 		cartService.clearCart(cartData);
-		return "order";
-	}
-	
+			return "order";
+	}	
 	
 	@RequestMapping(value = "/sellHistory", method = RequestMethod.GET)
 	public String getSellHistory(Model model, HttpServletRequest request) {
